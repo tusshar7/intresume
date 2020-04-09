0c59617a46f08c82e735b36dd62d3dcbe15f6357
unknown <tushargoyal7776@gmail.com>
2020-04-10 01:56:15 +0530

    Updates

---
+++ precache-manifest.96391b9005e2f9013bcbb8b948883c9e.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9254cb95e7918afdd1e9d431bd5aadf3",
+    "url": "/intresume/index.html"
+  },
+  {
+    "revision": "2e3ce1114d2016f3f3c8",
+    "url": "/intresume/static/css/main.10ef7870.chunk.css"
+  },
+  {
+    "revision": "d6dcd088b49ea6b3971d",
+    "url": "/intresume/static/js/2.0f7c6ad1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/intresume/static/js/2.0f7c6ad1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e3ce1114d2016f3f3c8",
+    "url": "/intresume/static/js/main.90900cc9.chunk.js"
+  },
+  {
+    "revision": "750eac5049fbab6ef060",
+    "url": "/intresume/static/js/runtime-main.af8a4600.js"
+  },
+  {
+    "revision": "e1eb3839799316360022791096bfa5d0",
+    "url": "/intresume/static/media/bck.e1eb3839.jpeg"
+  },
+  {
+    "revision": "d3c872e07e1e5a8fdaae7559f656a68a",
+    "url": "/intresume/static/media/contact.d3c872e0.jpg"
+  },
+  {
+    "revision": "c0e3d8674141ae0e5c323e6ec03d349f",
+    "url": "/intresume/static/media/fb.c0e3d867.jpg"
+  },
+  {
+    "revision": "2fb826baaaaddc53d56b042146f3f5e3",
+    "url": "/intresume/static/media/git.2fb826ba.jpg"
+  },
+  {
+    "revision": "48f09a1635ffbd8cfdd16bca625d50bb",
+    "url": "/intresume/static/media/gmail.48f09a16.jpg"
+  },
+  {
+    "revision": "bcc187962fdfb54f2eb7418d866184d4",
+    "url": "/intresume/static/media/home.bcc18796.jpg"
+  },
+  {
+    "revision": "fd9c2c90d90f16251fce5496479ae9d6",
+    "url": "/intresume/static/media/insta.fd9c2c90.PNG"
+  },
+  {
+    "revision": "c9d61531cd424d61b8b472af13cd8f43",
+    "url": "/intresume/static/media/intlink.c9d61531.jpg"
+  },
+  {
+    "revision": "c55819c4dba728837f134d044e01c6ea",
+    "url": "/intresume/static/media/profile.c55819c4.png"
+  },
+  {
+    "revision": "06f8d8fb4321def32818d1c6fe3797e5",
+    "url": "/intresume/static/media/project.06f8d8fb.jpg"
+  },
+  {
+    "revision": "f6a7901c221c646f18692b755db3b9fc",
+    "url": "/intresume/static/media/resume.f6a7901c.jpg"
+  },
+  {
+    "revision": "4b41a3475132bd861b30a878e30aa56a",
+    "url": "/intresume/static/media/sample.4b41a347.pdf"
+  },
+  {
+    "revision": "bf7e7d23c502af028d23a639c59e53e1",
+    "url": "/intresume/static/media/skills.bf7e7d23.jpg"
+  },
+  {
+    "revision": "9bf442b46510359870dab996bf6cf750",
+    "url": "/intresume/static/media/tush.9bf442b4.png"
+  }
+]);
